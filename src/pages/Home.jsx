e6a1a37da963f64f291010a935372a322d80760f
Michael McCormack <mcctwin@gmail.com>
2025-08-15 17:44:53 -0500

    Create outlet context of users and share with UserCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,16 @@
 import { useState, useEffect } from "react"
 import UserCard from "../components/UserCard"
 import NavBar from "../components/NavBar"
-import { Outlet } from "react-router-dom"
+import { Outlet, useOutletContext } from "react-router-dom"
 
 function Home() {
-  const [users, setUsers] = useState([])
+  const users = useOutletContext()
+  const userList = users.map(user => <UserCard key={user.id} user={user} />)
 
-  useEffect(() =>{
-    fetch("http://localhost:4000/users")
-      .then(r => {
-        if (!r.ok) { throw new Error("failed to fetch users") }
-        return r.json()
-      })
-      .then(data => setUsers(data))
-      .catch(error => console.error(error))
-  }, [])
-  
-  const userList = users.map(user => {
-    return <UserCard key={user.id} user={user}/>
-  });
+ 
 
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
       <main>
         <h1>Home!</h1>
         <Outlet />
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,25 @@
 import NavBar from "../components/NavBar"
 import { Outlet } from "react-router-dom"
-const Layout = () => {
-    return (
+import { useState, useEffect } from "react"
+function Layout(){
+    const [users, setUsers] = useState([])
+
+    useEffect(() =>{
+        fetch("http://localhost:4000/users")
+        .then(r => {
+            if (!r.ok) { throw new Error("failed to fetch users") }
+            return r.json()
+        })
+        .then(data => setUsers(data))
+        .catch(error => console.error(error))
+    }, [])
+
+    return(
         <>
             <header>
                 <NavBar />
             </header>
-            <Outlet />
+            <Outlet context={users}/>
         </>
     )
 }
